2b9a93ff52c6be9048d923e164e2b265b4179ba7
nduwrdc <dbensel@ndejjeuniversity.ac.ug>
2020-05-27 22:59:25 +0300

    #37 solved.

--- core/modules/lims/doc/pdf_lims_testreport.modules.php
+++ core/modules/lims/doc/pdf_lims_testreport.modules.php
@@ -626,9 +626,16 @@ class pdf_lims_testreport extends CommonDocGenerator
 
 					// Result
 					$result = $object->lines[$i]->result;
+
 					// check if min/max is set and $result outside min/max
 					if ( (!is_null($minimum) && $result < $minimum) || (!is_null($maximum) && $result > $maximum) )
 						$pdf->SetFont('', 'B', $default_font_size - 1); // Make bold if outside min/max
+					
+					// check if result outside of measurement range
+					if (!is_null($method->range_lower) && $result < $method->range_lower)
+						$result = '< '.$method->range_lower;
+					if (!is_null($method->range_upper) && $result > $method->range_upper)
+						$result = '> '.$method->range_upper;
 					
 					$pdf->SetXY($this->posxresult, $curY);
 					$pdf->MultiCell($this->posxunit - $this->posxresult, 3, $result, 0, 'C');
