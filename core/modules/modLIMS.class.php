0c64a56b06088be62dda62402fef0bdf69f9a9dd
nduwrdc <dbensel@ndejjeuniversity.ac.ug>
2020-05-29 12:09:56 +0300

    Changed version to 0.1, set ID to 207150, added some information

--- core/modules/modLIMS.class.php
+++ core/modules/modLIMS.class.php
@@ -45,7 +45,7 @@ class modLIMS extends DolibarrModules
 
         // Id for module (must be unique).
         // Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
-        $this->numero = 500000; // TODO Go on page https://wiki.dolibarr.org/index.php/List_of_modules_id to reserve an id number for your module
+        $this->numero = 207150;
         // Key text used to identify module (for permissions, menus, etc...)
         $this->rights_class = 'lims';
         // Family can be 'base' (core modules),'crm','financial','hr','projects','products','ecm','technic' (transverse modules),'interface' (link with external tools),'other','...'
@@ -61,10 +61,10 @@ class modLIMS extends DolibarrModules
         $this->description = "LIMSDescription";
         // Used only if file README.md and README-LL.md not found.
         $this->descriptionlong = "LIMS description (Long)";
-        $this->editor_name = 'Editor name';
-        $this->editor_url = 'https://www.example.com';
+        $this->editor_name = 'David Bensel';
+        $this->editor_url = 'https://www.nduwrdc.org';
         // Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated' or a version string like 'x.y.z'
-        $this->version = '1.0';
+        $this->version = '0.1';
         // Url to the file with your last numberversion of this module
         //$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
