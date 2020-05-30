e3ef58a6bb11cc5ea1b2e0ab1ebedd0adcad90ac
nduwrdc <dbensel@ndejjeuniversity.ac.ug>
2020-05-30 12:02:21 +0300

    validate sample was possible with user->rights->lims->samples->write. new $permissiontovalidate = user->rights->lims->samples->delete

--- samples_card.php
+++ samples_card.php
@@ -122,6 +122,7 @@ $usercancreate = $user->rights->lims->samples->write;
 
 $permissiontoread = $user->rights->lims->samples->read;
 $permissiontoadd = $user->rights->lims->samples->write; // Used by the include of actions_addupdatedelete.inc.php and actions_lineupdown.inc.php
+$permissiontovalidate = $user->rights->lims->samples->delete;
 $permissiontodelete = $user->rights->lims->samples->delete || ($permissiontoadd && isset($object->status) && $object->status == $object::STATUS_DRAFT);
 $permissionnote = $user->rights->lims->samples->write; // Used by the include of actions_setnotes.inc.php
 $permissiondellink = $user->rights->lims->samples->write; // Used by the include of actions_dellink.inc.php
@@ -726,7 +727,7 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 			// Back to draft
 			if ($object->status == $object::STATUS_VALIDATED)
 			{
-				if ($permissiontoadd)
+				if ($permissiontovalidate)
 				{
 					print '<a class="butAction" href="'.$_SERVER['PHP_SELF'].'?id='.$object->id.'&action=confirm_setdraft&confirm=yes">'.$langs->trans("SetToDraft").'</a>';
 				}
@@ -744,7 +745,7 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 			// Validate
 			if ($object->status == $object::STATUS_DRAFT)
 			{
-				if ($permissiontoadd)
+				if ($permissiontovalidate)
 				{
 					if (empty($object->table_element_line) || (is_array($object->lines) && count($object->lines) > 0))
 					{
