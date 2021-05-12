fc8e2050ea03288b73e5d587da7750c0c51e4425
David Bensel <david.bensel@gmail.com>
2021-05-12 16:42:56 +0300

    Set version to 0.2.1 in module description

--- core/modules/modLIMS.class.php
+++ core/modules/modLIMS.class.php
@@ -64,7 +64,7 @@ class modLIMS extends DolibarrModules
 		$this->editor_name = 'David Bensel';
 		$this->editor_url = 'https://www.nduwrdc.org';
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated' or a version string like 'x.y.z'
-		$this->version = '0.2.0';
+		$this->version = '0.2.1';
 		// Url to the file with your last numberversion of this module
 		//$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
